b5b055458a1fe81d13d64bf4ea5961c20a4db3cc
mario.murd@gmail.com <61310052+Hungwy@users.noreply.github.com>
2022-11-11 16:03:23 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import { CsvToHtmlTable } from "react-csv-to-table";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+//https://www.npmjs.com/package/react-csv-to-table
+//https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+//https://react-bootstrap.github.io/getting-started/introduction/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState("Lae csv fail");
+
+    let fileReader;
+
+    const onChange = e => {
+        let file = e.target.files;
+        fileReader = new FileReader();
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsText(file[0]);
+    };
+
+    const handleFileRead = e => {
+        let content = fileReader.result;
+        setText(content);
+    };
+    return (
+        <>
+            <div>
+                <input type="file" name="myfile" onChange={onChange} />
+            </div>
+            <CsvToHtmlTable
+                data={text}
+                csvDelimiter=","
+                tableClassName="table table-striped table-hover"
+            />
+        </>
+    );
 }
 
 export default App;
